729340dd805b1ae6222342440b7e336b8b9d18e5
KaitouK <lin1015268@gmail.com>
2026-06-25 16:39:53 +0800

    update migrations

--- Migrations/20260625083808_ChangesJournalColumnName.Designer.cs
+++ Migrations/20260625083808_ChangesJournalColumnName.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyPortfolio.Model;
 
@@ -10,9 +11,11 @@ using MyPortfolio.Model;
 namespace MyPortfolio.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260625083808_ChangesJournalColumnName")]
+    partial class ChangesJournalColumnName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.9");
---
+++ Migrations/20260625083808_ChangesJournalColumnName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyPortfolio.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangesJournalColumnName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LocalFilePath",
+                table: "JournalImages",
+                newName: "BlobName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BlobName",
+                table: "JournalImages",
+                newName: "LocalFilePath");
+        }
+    }
+}
